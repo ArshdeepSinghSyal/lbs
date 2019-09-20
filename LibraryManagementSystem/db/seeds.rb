c49dad5f2e50d7af46867f389b64caae9e2d15c5
Richa Dua <rdua2@ncsu.edu>
2019-09-20 18:23:49 -0400

    Added basic auth for admin

--- LibraryManagementSystem/db/seeds.rb
+++ LibraryManagementSystem/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user.create(:email => 'admin@lms.com', :password => 'password', :user_type => 1, :name => 'Administrator')
