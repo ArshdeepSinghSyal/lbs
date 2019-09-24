3e19b4c99dc2795c45372f3e9b7a4ca4feaefecc
Arshdeep Singh Syal <arshdeep2323@gmail.com>
2019-09-23 22:13:09 -0400

    removed admin scaffold

--- LibraryManagementSystem/config/routes.rb
+++ LibraryManagementSystem/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
-  resources :admins
   resources :books
   resources :libraries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
