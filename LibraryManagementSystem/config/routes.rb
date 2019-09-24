7d841d2341e8ab1ec212cd1fc73bc2ee649897c2
Arshdeep Singh Syal <arshdeep2323@gmail.com>
2019-09-23 21:51:51 -0400

    removed students scaffold

--- LibraryManagementSystem/config/routes.rb
+++ LibraryManagementSystem/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :admins
   resources :books
-  resources :students
   resources :libraries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
