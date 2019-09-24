1e0030b47e95d3f3aefb841a5fbf3146b67dc8f3
Arshdeep Singh Syal <arshdeep2323@gmail.com>
2019-09-23 22:45:12 -0400

    updated ruby version in gem file & removed unneccesary scafolds

--- LibraryManagementSystem/app/views/librarians/_librarian.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! librarian, :id, :created_at, :updated_at
-json.url librarian_url(librarian, format: :json)
--- LibraryManagementSystem/db/migrate/20190920204117_create_librarians.rb
+++
@@ -1,8 +0,0 @@
-class CreateLibrarians < ActiveRecord::Migration[5.2]
-  def change
-    create_table :librarians do |t|
-
-      t.timestamps
-    end
-  end
-end
