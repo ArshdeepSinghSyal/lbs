43a89cb800b4b2255a09d5c28cf2bab5d621653d
Arshdeep Singh Syal <arshdeep2323@gmail.com>
2019-09-25 20:11:55 -0400

    updated schema.rb

--- LibraryManagementSystem/db/schema.rb
+++ LibraryManagementSystem/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_211615) do
+ActiveRecord::Schema.define(version: 2019_09_25_213226) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2019_09_25_211615) do
   create_table "libraries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "university"
+    t.string "location"
+    t.string "max_days_undergrad"
+    t.string "max_days_grad"
+    t.string "max_days_phd"
   end
 
   create_table "students", force: :cascade do |t|
