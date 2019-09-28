44d26e6454d942b88ea5c18b15e38d4bd7649264
Richa Dua <rdua2@ncsu.edu>
2019-09-28 02:32:56 -0400

    Added University on UI

--- LibraryManagementSystem/app/models/university.rb
+++ LibraryManagementSystem/app/models/university.rb
@@ -1,3 +1,4 @@
 class University < ApplicationRecord
   has_many :libraries, dependent: :delete_all
+  has_many :users, dependent: :delete_all
 end
--- LibraryManagementSystem/app/models/user.rb
+++ LibraryManagementSystem/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :university
   validates :password, presence: true
   validates :usertype, presence: true
   validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create}
