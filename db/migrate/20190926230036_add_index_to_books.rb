36888f1f8831007db9aabe8fdae75c2ca3f762f0
Arshdeep Singh Syal <arshdeep2323@gmail.com>
2019-09-29 18:53:19 -0400

    commented out conflicting migrations

--- db/migrate/20190926230036_add_index_to_books.rb
+++ db/migrate/20190926230036_add_index_to_books.rb
@@ -1,6 +1,6 @@
 class AddIndexToBooks < ActiveRecord::Migration[5.2]
   def change
-    remove_column :books, :isbn
+    # remove_column :books, :isbn
     add_column :books, :isbn, :string
     add_index :books, :isbn, unique: true
   end
