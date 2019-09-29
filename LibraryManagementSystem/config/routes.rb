35b7a8e02fe08972758cc3553558c6df01563098
Arshdeep Singh Syal <arshdeep2323@gmail.com>
2019-09-29 16:39:19 -0400

    added deployment changes in gemfile & routes

--- LibraryManagementSystem/config/routes.rb
+++ LibraryManagementSystem/config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
+  root to: 'universities#index'
 end
