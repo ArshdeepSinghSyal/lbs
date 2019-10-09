1f935a7c957521f88a49ae4a27624ad85985856c
Arshdeep Singh Syal <arshdeep2323@gmail.com>
2019-10-09 02:07:49 -0400

    added tests for book model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,4 +4,16 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  # ######################## NEWLY ADDED TEST CASES ######################
+     test "should have valid name" do
+      b = Book.new
+      b.name = "Giuerh"
+      assert b.save
+    end
+
+    test "valid name not present" do
+      b = Book.new
+      assert_not b.save
+    end
 end
